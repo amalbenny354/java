4392e0832b4afbcd73f19b5ef850a80d69d92621
amal <116335467+amalbenny354@users.noreply.github.com>
2023-06-03 15:38:35 +0530

    Update methodoverloading.java

--- methodoverloading.java
+++ methodoverloading.java
@@ -1,43 +1,56 @@
 import java.util.*;
-
-class methodoverloading
-{
-	public static void main(String args[])
-	{
-		int side,len,brd;
-		Scanner input=new Scanner(System.in);
-		
-		System.out.println("enter length");
-		side=input.nextInt();
-		
-		System.out.println("enter length and bredth");
-		len=input.nextInt();
-		brd=input.nextInt();
-
-		A object=new A();
-		System.out.println("area of square is "+ object.area(side));
-		System.out.println("area of rectangle is "+ object.area(len,brd));
-	}
+class A{
+ int a,b;
+ double r;
+ int area(int a1){
+  this.a=a1;
+  return(a*a);
+ }
+ int area(int l,int b){
+  this.a=l;
+  this.b=b;
+  return(a*b);
+ }
+ double area(double r){
+  this.r=r;
+  return(r*r*3.14);
+ }
 }
-
-class A
-{
-	int a,b;
-	
-	int area(int side)
-	{
-		this.a=side;
-		return(side*side+);
-	}
-	
-	int area(int len,int brd)
-	{
-		this.a=len;
-		this.b=brd;
-		return(len*brd);
-	}
-}
-
-
-
-
+class Area{
+ public static void main(String args[]){
+  int a1,l,b,ch,x;
+  Scanner inpt=new Scanner(System.in);
+  Scanner inp=new Scanner(System.in);
+  double r;
+  A Obj=new A();
+  do{
+   System.out.println("\n M E N U \n");
+   System.out.println(" 1. Area of Square. ");
+   System.out.println(" 2. Area of Rectangle. ");
+   System.out.println(" 3. Area of Circle. ");
+   System.out.println(" 4. Exit ");
+   System.out.println(" Enter Your Choice [1-4] : \n");
+   ch=inpt.nextInt();
+   switch(ch){
+   case 1:System.out.println(" Enter Side of Square : ");
+          a1=inp.nextInt();
+          System.out.println(" Area Of Square : "+Obj.area(a1));
+          break;
+   case 2:System.out.println(" Enter Length and Breadth of Rectangle: ");
+          l=inp.nextInt();
+          b=inp.nextInt();
+          System.out.println(" Area Of Rectangle : "+Obj.area(l,b));
+          break;
+   case 3:System.out.println(" Enter Radius of Circle : ");
+          r=inp.nextDouble();
+          System.out.println(" Area Of Circle : "+Obj.area(r));
+          break;
+   case 4:System.out.println("\n\n T H A N K    Y O U \n");
+          break;   
+   default:System.out.println("\n Wrong Choice ");
+   }
+    System.out.println("\n Doy You Want to Continue [0/1] : ");
+    x=inpt.nextInt();
+  }while(x==1);                 
+ }  
+}  
