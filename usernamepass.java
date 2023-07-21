d6f93eaeaf2c03aae258a4cb9478afc9f7220ce1
amal <116335467+amalbenny354@users.noreply.github.com>
2023-07-21 10:59:46 +0530

    Add files via upload

---
+++ usernamepass.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.lang.Exception;
+
+class UsernameException extends Exception{
+	UsernameException (String msg)
+	{
+		System.out.println("msg");
+	}
+}
+
+class PasswordException extends Exception{
+	PasswordException(String msg)
+	{
+		System.out.println("msg");
+	}
+}
+
+public class usernamepass
+{
+	public static void main(String args[])
+	{
+		Scanner reader=new Scanner(System.in);
+		String username,password;
+		System.out.println("Enter Username");
+		username=reader.nextLine();
+		System.out.println("Enter Password");
+		password=reader.nextLine();
+		int len=username.length();
+		try
+		{
+		
+			if( len > 8)
+			{
+				throw new UsernameException("username must be greater than 8");
+			}
+
+			else if(!password.equals("admin"))
+			{
+				throw new PasswordException("password incorrect");
+			}
+			else
+			{
+				System.out.println("login successfull");
+			}
+		}
+		catch(UsernameException u)
+		{
+			System.out.println("login successful");
+		}
+		catch(PasswordException p)
+		{
+			System.out.println("login succesful");
+		}
+	}
+}
